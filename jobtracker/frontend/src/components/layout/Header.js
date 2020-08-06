b84ea13ca4a5472d80db46ddb4e1d52d240e1b32
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-08-06 02:58:15 -0500

    class -> className in Header.js

--- jobtracker/frontend/src/components/layout/Header.js
+++ jobtracker/frontend/src/components/layout/Header.js
@@ -33,13 +33,13 @@ export class Header extends Component {
     );
 
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Job Tracker
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-toggle="collapse"
             data-target="#navbarNavAltMarkup"
@@ -47,10 +47,10 @@ export class Header extends Component {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
               <Link to="/" className="nav-link mr-4">
                 Home
               </Link>
@@ -71,53 +71,6 @@ export class Header extends Component {
           </div>
         </div>
       </nav>
-
-      /*  <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="#">
-              Job Tracker
-            </a>
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/new-job" className="nav-link">
-                  Add Job
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/jobs" className="nav-link">
-                  Jobs List
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="http://jobtracker-django.herokuapp.com/"
-                >
-                  Job Tracker - Django Frontend
-                </a>
-              </li>
-            </ul>
-            {isAuthenticated ? authLinks : guestLinks}
-          </div>
-        </div>
-      </nav> */
     );
   }
 }
