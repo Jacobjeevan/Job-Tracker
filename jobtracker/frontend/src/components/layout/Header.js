50dd58bd49b2be1470b2a4e99d295d6e5153c3f6
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-08-05 22:56:35 -0500

    Navbar modifications
    - Cleaned up the navbar (width for responsive design)

--- jobtracker/frontend/src/components/layout/Header.js
+++ jobtracker/frontend/src/components/layout/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
+import "./Header.css";
 
 export class Header extends Component {
   static propTypes = {
@@ -13,32 +14,65 @@ export class Header extends Component {
   render() {
     const { isAuthenticated, user } = this.props.auth;
     const authLinks = (
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link to="/login" onClick={this.props.logout} className="nav-link">
-            Logout
-          </Link>
-        </li>
+      <ul className="navbar-nav mt-2 mt-lg-0 auth-nav">
+        <Link to="/login" onClick={this.props.logout} className="nav-link mr-4">
+          Logout
+        </Link>
       </ul>
     );
 
     const guestLinks = (
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link to="/register" className="nav-link">
-            Register
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            Login
-          </Link>
-        </li>
+      <ul className="navbar-nav mt-2 mt-lg-0 auth-nav">
+        <Link to="/register" className="nav-link mr-4">
+          Register
+        </Link>
+        <Link to="/login" className="nav-link mr-4">
+          Login
+        </Link>
       </ul>
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a class="navbar-brand" href="#">
+            Job Tracker
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link to="/" className="nav-link mr-4">
+                Home
+              </Link>
+              <Link to="/new-job" className="nav-link mr-4">
+                Add Job
+              </Link>
+              <Link to="/jobs" className="nav-link mr-4">
+                Jobs List
+              </Link>
+              <a
+                className="nav-link mr-4"
+                href="http://jobtracker-django.herokuapp.com/"
+              >
+                Django Frontend Version
+              </a>
+              {isAuthenticated ? authLinks : guestLinks}
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      /*  <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
           <button
             className="navbar-toggler"
@@ -83,7 +117,7 @@ export class Header extends Component {
             {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
-      </nav>
+      </nav> */
     );
   }
 }
