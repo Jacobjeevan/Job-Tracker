8d34aa1dca3e5a7c192e0cdfc0b3efbc2635bd09
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2020-12-17 15:53:51 -0600

    UserRepo
    - Added functions to query the database
        - Create new User
            - Helper fn to hash password
        - Find user by Email

---
+++ jobtracker/backend/server/components/User/UserRepo.js
@@ -0,0 +1,35 @@
+const User = require("./UserModel"),
+  bcrypt = require("bcrypt");
+
+async function getUserByEmail(emailId) {
+  try {
+    let user = await User.findOne({
+      where: {
+        email: emailId,
+      },
+    });
+    return user ? user : null;
+  } catch (error) {
+    throw new Error(`Could not find if user exists - ${error}`);
+  }
+}
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+}
+
+async function createNewUser(params) {
+  try {
+    const user = await User.create(params);
+    return user ? user : null;
+  } catch (error) {
+    throw new Error(`Could not create new user - ${error}`);
+  }
+}
+
+module.exports = {
+  getUserByEmail,
+  hashPassword,
+  createNewUser,
+};
