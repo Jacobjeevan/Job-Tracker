bbc5371212162f23ed9d2cab1258715da95dba37
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2020-12-17 15:51:44 -0600

    TokenHandler and ErrorHandler
    - Added various functions for generating and verifying tokens
        - getTokenFromHeader
        - generateToken
        - verifyToken
    - Added a errorHandler fn to return JSON objects with error Messages

---
+++ jobtracker/backend/server/utils/tokenHandler.js
@@ -0,0 +1,55 @@
+var nJwt = require("njwt");
+const signingKey = process.env.TOKEN_SECRET;
+const { handleError } = require("./errors");
+
+const claims = (userId, isDefaultUser) => {
+  let scope = "user";
+  if (isDefaultUser) scope = "testUser";
+  return {
+    iss: `${BACKEND_URL}`,
+    sub: userId,
+    scope,
+  };
+};
+
+const generateToken = (userId, isDefaultUser) => {
+  const tokenClaim = claims(userId, isDefaultUser);
+  var jwt = nJwt.create(tokenClaim, signingKey);
+  var token = jwt.compact();
+  return token;
+};
+
+const getTokenFromHeader = (req) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Token"
+  ) {
+    let token = req.headers.authorization.split(" ")[1];
+    return token ? token : null;
+  }
+};
+
+const verifyToken = (req, res, next) => {
+  const token = getTokenFromHeader(req);
+  if (token) {
+    nJwt.verify(token, signingKey, function (err, verifiedJwt) {
+      if (err) {
+        console.log(err); // Token has expired, has been tampered with, etc
+        handleError(res, 404, "Token could be validated. Please login again.");
+      } else {
+        const { scope } = verifiedJwt.body;
+        req.body.scope = scope;
+        console.log(verifiedJwt); // Will contain the header and body
+        next();
+      }
+    });
+  } else {
+    return handleError(res, 404, "Token not found in header.");
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+  getTokenFromHeader,
+};
