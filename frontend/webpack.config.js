fb31a7be28725f6312c2f5b5339ab6e453953f1e
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-01-31 20:40:23 -0600

    Parcel -> Webpack
    - Switched from Parcel back to Webpack
    
    Initially I moved away from webpack since I wanted to experiment with Parcel; however I wanted to use Tailwind for frontend
    - Couldn't get Tailwind + Parcel to work well
    	- Had to use a nightly version of parcel to get it working (which wasn't reliable because of occassional compilation errors)
    - So switched back to webpack for compiling frontend files
    - Installed css, style and postcss loaders
    - Installed tailwind
    - Added tailwind css support (App.css)
    
    Next step: Beautify Frontend with Tailwind!

---
+++ frontend/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+  },
+  plugins: [new Dotenv(), new webpack.HotModuleReplacementPlugin()],
+};
