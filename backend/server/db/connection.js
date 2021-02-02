2dedb4d7cc8f8ebf823aca7ba8304adb8b3ec565
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-02-02 17:04:35 -0600

    DB URL
    - Changed DB Url to reflect Dokku setup
    - Additional logging if DB connection fails
    - Added .env.* to gitignore

--- backend/server/db/connection.js
+++ backend/server/db/connection.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const dbConnection = new Sequelize(
-  `${process.env.POSTGRES_DB}`,
-  `${process.env.POSTGRES_USER}`,
-  `${process.env.POSTGRES_PASSWORD}`,
-  {
-    host: `${process.env.POSTGRES_HOST}`,
-    port: `${process.env.POSTGRES_PORT}`,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const dbConnection = new Sequelize(`${process.env.DATABASE_URL}`, {
+  dialect: "postgres",
+  logging: false,
+});
 
 module.exports = dbConnection;
--- backend/server/db/dbHelper.js
+++ backend/server/db/dbHelper.js
@@ -4,14 +4,14 @@ const dbConnection = require("./connection");
 const connectDB = async () => {
   try {
     await dbConnection.authenticate();
-    logger.info(
-      `Succesfully connected to Database on port: ${process.env.POSTGRES_PORT}`
-    );
+    logger.info("Succesfully connected to Database on port");
     if (process.env.NODE_ENV !== "TEST") {
       await dbConnection.sync({ alter: true });
       logger.info("Synchronized DB");
     }
   } catch (error) {
+    logger.debug(`${process.env.DATABASE_URL}`);
+    logger.error(error);
     logger.error("Unable to connect to Database.");
   }
 };
